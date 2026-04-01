258b742399ff2c6ec9a543cf4ea844ae53403f3f
Shubh Pingale <135618936+shubhtrek@users.noreply.github.com>
2026-04-01 12:00:46 +0530

    Database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"log"
+	"todo-app/internal/config"
+	"todo-app/internal/database"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
+
+	var cfg *config.Config
+	var err error
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	defer pool.Close()
+
 	var router *gin.Engine
 
 	router = gin.Default()
@@ -13,9 +36,11 @@ func main() {
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
-			"message": "Server Running",
+			"message": "Todo API is running well!",
+			"status": "success",
+			"database": "connected",
 		})
 	})
 
-	router.Run(":8080")
+	router.Run(":"+ cfg.Port)
 }
